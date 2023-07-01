6762b048f51d03270bd1973e126f4912c21a7dac
Ar-Ray <67567093+Ar-Ray-code@users.noreply.github.com>
2023-07-01 09:07:35 +0900

    Update aivtuber_nao.launch.py

--- aivtuber_bringup/launch/aivtuber_nao.launch.py
+++ aivtuber_bringup/launch/aivtuber_nao.launch.py
@@ -1,93 +1,65 @@
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import (EmitEvent, ExecuteProcess, IncludeLaunchDescription,
-                            LogInfo, RegisterEventHandler, TimerAction)
-from launch.event_handlers import (OnProcessExit, OnProcessIO, OnProcessStart, OnShutdown)
-from launch_ros.actions import Node
-from launch.events import Shutdown
 import os
+import sys
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, TimerAction
+from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # launch demo_nodes_cpp
-    launch_share_dir = os.path.join(get_package_share_directory('demo_nodes_cpp'), 'launch')
-    talker_listener_launch = IncludeLaunchDescription(
-        launch_description_source=os.path.join(launch_share_dir, 'topics/talker_listener.launch.py')
+    youtube_id = ''
+    workspace = '~/ws_aivtuber'
+    for arg in sys.argv:
+        if 'youtube_id' in arg:
+            youtube_id = arg.split('youtube_id:=')[1]
+        if 'workspace' in arg:
+            workspace = arg.split('workspace:=')[1]
+
+
+    if youtube_id == '':
+        print('Please specify youtube_id')
+        exit()
+    else:
+        if 'watch?v=' in youtube_id:
+            youtube_id = youtube_id.split('watch?v=')[1]
+        if '&' in youtube_id:
+            youtube_id = youtube_id.split('&')[0]
+        video_id = youtube_id
+
+    print("============== args ================")
+    print("workspace: " + workspace)
+    print("video_id: " + video_id)
+    print("====================================")
+
+
+    # Start simulation
+    start_sim_cmd = ExecuteProcess(
+        cmd=['bash', os.path.expanduser(workspace) + '/src/ROS2-AI-VTuber-Projects/webots_world/nao/start_sim.bash'],
+        shell=True,
+        cwd=os.path.expanduser(workspace)
     )
 
-    # xeyes
-    xeyes_cmd = ExecuteProcess(
-        cmd=['xeyes']
-    )
-    # node
-    turtlesim_node = Node(package='turtlesim', executable='turtlesim_node')
-
-
-    # OnProcessIO for talker
-    turtlesim_node_io = RegisterEventHandler(
-        OnProcessIO(
-            target_action=turtlesim_node,
-            on_stdout=lambda event: LogInfo(
-                    msg='Spawn request says "{}"'.format(
-                        event.text.decode().strip())
-                ),
-            on_stderr=lambda event: LogInfo(
-                    msg='Spawn request says "{}"'.format(
-                        event.text.decode().strip())
-                ),
-        )
+    # Run voicevox_engine container
+    voicevox_engine_cmd = ExecuteProcess(
+        cmd=['docker', 'run', '--rm', '-p', '127.0.0.1:50021:50021', 'voicevox/voicevox_engine:cpu-ubuntu20.04-latest'],
+        shell=True
     )
 
-    xeyes_delay = TimerAction(period=3.0, actions=[xeyes_cmd])
-    turtlesim_after_xeyes = RegisterEventHandler(
-        OnProcessExit(
-            target_action=xeyes_cmd,
-            on_exit=[
-                LogInfo(msg="==== xeyes exited ===="),
-                turtlesim_node
-            ]
-        )
-    )
+    print("============== nodes ================")
+    speak_ros = Node(package='speak_ros', executable='speak_ros_node', parameters=[{'plugin_name': 'voicevox_plugin::VoiceVoxPlugin'}])
+    motion_socket = Node(package='motion_socket', executable='motion_socket_node')
+    chatgpt_socket = Node(package='chatgpt_socket', executable='chatgpt_socket_node',
+        parameters=[{'ros_installed_setup_bash': os.path.expanduser(workspace) + '/install/setup.bash'},
+                    {'text_path': os.path.expanduser(workspace) + '/src/ROS2-AI-VTuber-Projects/chatgpt_socket/config/prompt.txt'},
+                    {'speak_enable': True}])
+    youtube_comment = Node(package='youtube_comment', executable='youtube_comment_node', parameters=[{'video_id': video_id}])
+    print("============== timer ================")
+    delay_node = TimerAction(period=10.0, actions=[speak_ros, motion_socket, chatgpt_socket, youtube_comment])
 
+    ld.add_action(start_sim_cmd)
+    ld.add_action(voicevox_engine_cmd)
 
-    shutdown_action = RegisterEventHandler(
-        OnShutdown(
-            on_shutdown=[LogInfo(msg="==== Shutdown ====")]
-        )
-    )
-
-
-    start_turtlesim_action = RegisterEventHandler(
-        OnProcessStart(
-            target_action=turtlesim_node,
-            on_start=[
-                LogInfo(msg="==== turtlesim_node started ====")
-            ]
-        )
-    )
-    end_turtlesim_action = RegisterEventHandler(
-        OnProcessExit(
-            target_action=turtlesim_node,
-            on_exit=[
-                LogInfo(msg="==== turtlesim_node exited ===="),
-                EmitEvent(event=Shutdown(
-                    reason='turtlesim_node exited'
-                ))
-            ]
-        )
-    )
-
-    ld.add_action(talker_listener_launch)
-    ld.add_action(xeyes_delay)
-
-    # ==== turtlesim_node ====
-    ld.add_action(turtlesim_after_xeyes)
-    ld.add_action(start_turtlesim_action)
-    ld.add_action(turtlesim_node_io)
-    ld.add_action(end_turtlesim_action)
-    # ==== end turtlesim_node ====
-
-    ld.add_action(shutdown_action)
+    ld.add_action(delay_node)
 
     return ld
